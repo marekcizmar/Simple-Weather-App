3e0ff1edbdcbd93fb3eee3d8bd7e28856f21c3c8
Marek Cizmar <marekcizmar@outlook.sk>
2022-11-17 17:25:38 +0100

    getting weather information

--- src/forecast.js
+++ src/forecast.js
@@ -1,5 +1,19 @@
 const key = "JGfirySRZaohz6f7hj1JHgkmDb6zsAUj";
 
+//get weather information
+const getWeather = async (id) => {
+
+    const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return(data[0]);
+
+}
+
+//get city information
 const getCity = async (city) => {
 
     const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
@@ -12,6 +26,8 @@ const getCity = async (city) => {
 
 }
 
-getCity("manchester")
-    .then(data => console.log(data))
-    .catch(err => console.log(err));
+getCity("manchester").then(data => {
+    return getWeather(data.Key)
+}).then(data => {
+    console.log(data);
+}).catch(err => console.log(err));
